a218338261e95592d9823cc2b557862630419322
Khushi Jaiswal <104351996+KHUSHIJAISWAL2608@users.noreply.github.com>
2023-07-19 23:49:12 +0530

    Add files via upload

---
+++ pract5.js
@@ -0,0 +1,12 @@
+let b = Math.random();
+let a=prompt("Enter the value you want to add in the array");
+a=Number.parseInt(a);
+if (a <b){
+    console.log("a<b");
+}
+else if(a>b){
+    console.log("a>b");
+}
+else{
+    console.log("a=b",a);
+}
